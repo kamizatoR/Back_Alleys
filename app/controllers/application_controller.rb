7d81739320c16c99780ae61b833f327cba9ddd4c
KamizatoR <112645912+kamizatoR@users.noreply.github.com>
2023-02-18 07:59:51 +0000

    [Fix]end_uesr_controller timeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def authenticate_any!
@@ -17,10 +18,15 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_kana, :first_kana, :display_name])
   end
-  
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
+
 end
