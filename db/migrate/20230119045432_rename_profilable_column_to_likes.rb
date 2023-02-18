e7d610752368c04b07c5544f716a64c0e905ae41
KamizatoR <112645912+kamizatoR@users.noreply.github.com>
2023-02-18 08:10:24 +0000

    fix migration

--- db/migrate/20230119045432_rename_profilable_column_to_likes.rb
+++ db/migrate/20230119045432_rename_profilable_column_to_likes.rb
@@ -2,7 +2,6 @@ class RenameProfilableColumnToLikes < ActiveRecord::Migration[6.1]
   def change
     rename_column :likes, :profilable_type, :table_type
     rename_column :likes, :profilable_id, :table_id
-    rename_column :likes, :end_user_id, :end_user_id, null: false
-
+    change_column_null :likes, :end_user_id, false
   end
 end
